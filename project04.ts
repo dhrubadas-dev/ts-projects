c2369790989600e55577cebd0da548f3d0316eca
Dhruba Das <dhrubadas.dev@gmail.com>
2026-02-05 13:41:21 +0530

    Project 04 Complete

---
+++ project04.ts
@@ -0,0 +1,11 @@
+const userObject = {
+	fullName: "Dhruba Das",
+	age: "18",
+	gender: "Male",
+	phoneNumber: "6297781575",
+};
+
+console.log(`Full Name: ${userObject.fullName}`);
+console.log(`Age: ${userObject.age}`);
+console.log(`Gender: ${userObject.gender}`);
+console.log(`Phone Number: ${userObject.phoneNumber}`);
