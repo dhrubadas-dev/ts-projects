d09c57c08c1fd05a5e0d43e310653672e4f30794
Dhruba Das <dhrubadas.dev@gmail.com>
2026-02-05 13:34:27 +0530

    Project 03 Complete

---
+++ project03.ts
@@ -0,0 +1,6 @@
+const userArray = ["Dhruba Das", "18", "Male", "6297781575"];
+
+console.log(`Full Name: ${userArray[0]}`);
+console.log(`Age: ${userArray[1]}`);
+console.log(`Gender: ${userArray[2]}`);
+console.log(`Phone Number: ${userArray[3]}`);
