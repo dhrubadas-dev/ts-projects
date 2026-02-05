b6482e4bf020d301aeaee19e99a851ab83d7af7a
Dhruba Das <dhrubadas.dev@gmail.com>
2026-02-05 19:44:49 +0530

    Project 06 Complete

---
+++ project06.ts
@@ -0,0 +1,15 @@
+let discount = 10;
+let laptopPrice = 30000;
+let headphonePrice = 2500;
+let speakerPrice = 1000;
+
+console.log(`Flat Discount Rate: ${discount}%`);
+console.log(
+	`Laptop: ${laptopPrice}/- (Discount: - ${(laptopPrice * discount) / 100}/-)`,
+);
+console.log(
+	`Headphone: ${headphonePrice}/- (Discount: - ${(headphonePrice * discount) / 100}/-)`,
+);
+console.log(
+	`Speaker: ${speakerPrice}/- (Discount: - ${(speakerPrice * discount) / 100}/-)`,
+);
