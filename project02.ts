e8dbf3945da2592c358ccdbf3660d892610f06d7
Dhruba Das <dhrubadas.dev@gmail.com>
2026-02-05 13:24:31 +0530

    Project 02 Complete

---
+++ project02.ts
@@ -0,0 +1,9 @@
+let fullName = "Dhruba Das";
+let age = "18";
+let gender = "Male";
+let phoneNumber = "6297781575";
+
+console.log(`Full Name: ${fullName}`);
+console.log(`Age: ${age}`);
+console.log(`Gender: ${gender}`);
+console.log(`Phone Number: ${phoneNumber}`);
